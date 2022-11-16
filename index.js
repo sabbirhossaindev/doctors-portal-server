e488642599a2bed1520d514e919ce5b57ae82c65
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-17 03:17:12 +0600

    booking done

--- index.js
+++ index.js
@@ -41,10 +41,69 @@ async function run() {
             res.send(options);
         });
 
-        app.post('/bookings', async (req, res) => {
+        // mongodb operator pipeline....
+        app.get('/v2/appointmentOptions', async (req, res) => {
             const date = req.query.date;
-            console.log(date);
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        });
+
+
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment 
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length){
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({acknowledged: false, message})
+            }
+
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })

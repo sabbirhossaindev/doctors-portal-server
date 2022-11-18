e17d463794c757bc3ba21b8a0e05c84863174fba
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-18 20:05:05 +0600

    user and email set up

--- index.js
+++ index.js
@@ -159,6 +159,27 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
         /***
 
          * .......API Naming Convention for bookings ......
